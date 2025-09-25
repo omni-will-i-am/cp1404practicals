6c7b2c17adaa84366af1aca53e8512fad6d702a1
William <william.neill@my.jcu.edu.au>
2025-09-25 23:31:18 +1000

    Add conversion functions for temperature units and wrap in main

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -6,19 +6,32 @@ Program for temperature conversion
 MENU = """C - Convert Celsius to Fahrenheit
 F - Convert Fahrenheit to Celsius
 Q - Quit"""
-print(MENU)
-choice = input(">>> ").upper()
-while choice != "Q":
-    if choice == "C":
-        celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
-        print(f"Result: {fahrenheit:.2f} F")
-    elif choice == "F":
-        fahrenheit = float(input("Fahrenheit : "))
-        celsius = 5 / 9 * (fahrenheit - 32)
-        print(f"Result: {celsius:.2f} C")
-    else:
-        print("Invalid option")
+
+def convert_f_to_c(fahrenheit: float) -> float:
+    celsius = 5 / 9 * (fahrenheit - 32)
+    return celsius
+
+def convert_c_to_f(celsius: float) -> float:
+    fahrenheit = celsius * 9.0 / 5 + 32
+    return fahrenheit
+
+def main():
     print(MENU)
     choice = input(">>> ").upper()
-print("Thank you.")
+    while choice != "Q":
+        if choice == "C":
+            celsius = float(input("Celsius: "))
+            fahrenheit = convert_c_to_f(celsius)
+            print(f"Result: {fahrenheit:.2f} F")
+        elif choice == "F":
+            fahrenheit = float(input("Fahrenheit : "))
+            celsius = convert_f_to_c(fahrenheit)
+            print(f"Result: {celsius:.2f} C")
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+if __name__ == "__main__":
+    main()
