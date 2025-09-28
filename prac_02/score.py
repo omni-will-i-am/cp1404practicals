9c8302ab3e3b2ad467856d228d72dd7639cb09ec
William <william.neill@my.jcu.edu.au>
2025-09-28 15:44:13 +1000

    Separate code into main function for input/output and new function to classify score and return result

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,12 +3,20 @@ CP1404/CP5632 - Practical
 Program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+def score_parameter(score: float):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+def main():
+    score = float(input("Enter score: "))
+    result = score_parameter(score)
+    print(result)
+
+if __name__ == "__main__":
+    main()
