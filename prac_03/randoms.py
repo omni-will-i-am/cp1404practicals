0ad20d5e622321dfa2e68d95c37533b7ee35a6f7
William <william.neill@my.jcu.edu.au>
2025-10-05 16:51:23 +1000

    Complete random number generator

---
+++ prac_03/randoms.py
@@ -0,0 +1,17 @@
+# What did you see on line 1?
+# What was the smallest number you could have seen, what was the largest?
+# Not sure what is meant by "what did you see", but the smallest and largest numbers seen were 5 and 20, respectively.
+
+# What did you see on line 2?
+# What was the smallest number you could have seen, what was the largest?
+# Could line 2 have produced a 4?
+# The smallest number would likely be 3, with the largest being 9. Producing a 4 is not possible with these instructions as the output (increments of 2) will only produce odd numbers.
+
+# What did you see on line 3?
+# What was the smallest number you could have seen, what was the largest?
+# Technically the smallest and largest number that can be printed here would be 2.5 and 5.5 respectively, although the chances of that happening are astronomically low.
+
+# Write code, not a comment, to produce a random number between 1 and 100 inclusive.
+
+import random
+print(random.randint(1, 100))
