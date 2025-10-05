3363bcad735dee8cf0d009970570d75708c64cce
William <william.neill@my.jcu.edu.au>
2025-10-05 19:23:20 +1000

    Add length validation method; Add character count validation; Add check for special characters and validation method

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -3,9 +3,9 @@ CP1404/CP5632 - Practical
 Password checker "skeleton" code to help you get started
 """
 
-MIN_LENGTH = 2
-MAX_LENGTH = 6
-IS_SPECIAL_CHARACTER_REQUIRED = False
+MIN_LENGTH = 5
+MAX_LENGTH = 15
+IS_SPECIAL_CHARACTER_REQUIRED = True
 SPECIAL_CHARACTERS = "!@#$%^&*()_-=+`~,./'[]<>?{}|\\"
 
 
@@ -27,20 +27,35 @@ def main():
 
 def is_valid_password(password):
     """Determine if the provided password is valid."""
-    # TODO: if length is wrong, return False
+    # if length is wrong, return False
+    if not (MIN_LENGTH <= len(password) <= MAX_LENGTH):
+        return False
 
     number_of_lower = 0
     number_of_upper = 0
     number_of_digit = 0
     number_of_special = 0
     for character in password:
-        # TODO: count each kind of character (use str methods like isdigit)
+        # count each kind of character (use str methods like isdigit)
+        if character.islower():
+            number_of_lower += 1
+        elif character.isupper():
+            number_of_upper += 1
+        elif character.isdigit():
+            number_of_digit += 1
+        elif character in SPECIAL_CHARACTERS:
+            number_of_special += 1
         pass
 
-    # TODO: if any of the 'normal' counts are zero, return False
+    # if any of the 'normal' counts are zero, return False
+    if number_of_lower == 0 or number_of_upper == 0 or number_of_digit == 0:
+        return False
 
-    # TODO: if special characters are required, then check the count of those
+    # if special characters are required, then check the count of those
     # and return False if it's zero
+    if IS_SPECIAL_CHARACTER_REQUIRED:
+        if number_of_special == 0:
+            return False
 
     # if we get here (without returning False), then the password must be valid
     return True
