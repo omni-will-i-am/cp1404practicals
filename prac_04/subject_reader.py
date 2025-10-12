80e0f5f751ff896279558b9190225b932d27b817
William <william.neill@my.jcu.edu.au>
2025-10-12 22:39:01 +1000

    Rename load_data to load_subjects & data to subjects. Return nested list and add display function. Improve naming and input/output.

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,23 +7,37 @@ FILENAME = "subject_data.txt"
 
 
 def main():
-    data = load_data(FILENAME)
-    print(data)
+    subjects = load_subjects(FILENAME)
+    display_subjects(subjects)
 
 
-def load_data(filename=FILENAME):
-    """Read data from file formatted like: subject,lecturer,number of students."""
-    input_file = open(filename)
-    for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
-        line = line.strip()  # Remove the \n
-        parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
-        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
-    input_file.close()
+def load_subjects(filename=FILENAME):
+    """Read data from file formatted like: subject,lecturer,number of students.
+    Return a nested list: [[code, lecturer, students], ...]"""
+    # in_file = open(filename)
+    # for line in in_file:
+    #     print(line)  # See what a line looks like
+    #     print(repr(line))  # See what a line really looks like
+    #     line = line.strip()  # Remove the \n
+    #     parts = line.split(',')  # Separate the data into its parts
+    #     print(parts)  # See what the parts look like (notice the integer is a string)
+    #     parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
+    #     print(parts)  # See if that worked
+    #     print("----------")
+    # in_file.close()
+    records = []
+    with open(filename) as in_file:
+        for line in in_file:
+            parts = line.strip().split(",")
+            parts[2] = int(parts[2])
+            records.append(parts)
+    return records
 
 
-main()
+def display_subjects(subjects):
+    for code, lecturer, students in subjects:
+        print(f"{code} is taught by {lecturer} and has {students} students.")
+
+
+if __name__ != "__main__":
+    main()
