b80619e60532f466037ddad778f30e400b0f058a
William <william.neill@my.jcu.edu.au>
2025-10-19 11:59:43 +1000

    Change to use exceptions and EAFP approach, complete prac 5 state  names exercise

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -4,7 +4,6 @@ State names in a dictionary
 File needs reformatting
 """
 
-# TODO: Reformat this file so the dictionary code follows PEP 8 convention
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
 
@@ -13,8 +12,9 @@ for code, name in sorted(CODE_TO_NAME.items()):
 
 state_code = input("Enter short state: ").upper()
 while state_code != "":
-    if state_code in CODE_TO_NAME:
-        print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
+    try:
+        state_name = CODE_TO_NAME[state_code]
+        print(f"{state_code} is {state_name}")
+    except KeyError:
         print("Invalid short state")
     state_code = input("Enter short state: ").upper()
