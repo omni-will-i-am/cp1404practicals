aa1fda28da612cce82d417aa12757d4e17024247
William <william.neill@my.jcu.edu.au>
2025-10-19 11:53:23 +1000

    Add string method for lowercase input and neatly aligned keys/values

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,11 @@
+"""
+CP1404/CP5632 Practical
+Program that allows user to look up hexadecimal colours.
+
+Use a constant dictionary of about 10 colour names and write a program that allows a user to enter a name and get the code,
+e.g., entering AliceBlue (or aliceblue - make it case-independent) should show #f0f8ff.
+
+Entering an invalid colour name should not crash the program.
+Allow the user to enter names until they enter a blank one to stop the loop.
+
+"""
