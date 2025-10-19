8b7caf2b508df5cf4ba46a43bab90846ca94ce26
William <william.neill@my.jcu.edu.au>
2025-10-19 22:21:45 +1000

    Start week 05 wimbledon practical exercise

---
+++ prac_05/wimbledon.py
@@ -0,0 +1,11 @@
+"""
+CP1404/CP5632 Practical 05
+Wimbledon gentlemen's singles champions data processing program
+
+This program will read a file, process the data and display the following processed information:
+    the champions and how many times they have won.
+    the countries of the champions in alphabetical order
+
+Estimate: 40 minutes
+Actual: n minutes
+"""
