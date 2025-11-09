4c8b753845c3cb090438ba84782c16ef09c287c1
William <william.neill@my.jcu.edu.au>
2025-11-09 22:11:15 +1000

    Complete initial construction of Project class for do-from-scratch project management exercise

---
+++ prac_07/project.py
@@ -0,0 +1,27 @@
+
+
+from datetime import date
+
+class Project:
+    """Represent a project object with name, start date, priority, cost estimate, and completion percentage."""
+
+    def __init__(self, name, start_date, priority, cost, completion_percentage):
+        """Construct a project from the given values."""
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost = cost
+        self.completion_percentage = completion_percentage
+
+    def __str__(self):
+        """Return a string representation of the project."""
+        return (f"{self.name}, start: {self.start_date.strftime('%d/%m/%Y')}, "
+                f"priority: {self.priority}, estimated cost: ${self.cost}, "
+                f"completion: {self.completion_percentage}%")
+
+    def __lt__(self, other):
+        """Sort projects ascending by priority."""
+        return self.priority < other.priority
+
+    def is_complete(self):
+        return self.completion_percentage >= 100
---
+++ prac_07/project_management.py
@@ -0,0 +1,8 @@
+
+
+import csv
+from datetime import datetime
+from prac_07.project import Project
+
+DATE_FORMAT = "%d/%m/%Y"
+DEFAULT_FILE = "projects.txt"
